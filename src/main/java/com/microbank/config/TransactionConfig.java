e5381de871fb48a2a639990796cc8de0578e0c9a
cozakoo <martinarcosvargas2@gmail.com>
2026-04-05 12:03:05 -0300

    Req 014 - configuracion postgreSQL y habilitacion de administrador de transacciones

---
+++ src/main/java/com/microbank/config/TransactionConfig.java
@@ -0,0 +1,21 @@
+package com.microbank.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class TransactionConfig {
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        DataSourceTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
+        transactionManager.setDefaultTimeout(30);
+        return transactionManager;
+    }
+}
